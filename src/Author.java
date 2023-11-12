4359631302b22af718fd84b6f564a30c94d823e4
JambaWAT <Puskinalex38@gmail.com>
2023-11-12 16:20:55 +0300

    a

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+   private String firstName;
+   private String Surname;
+
+    public Author(String firstName, String surname) {
+        this.firstName = firstName;
+        this.Surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Author azimov = new Author("Айзек", "Азимов");
+        Author clarke = new Author("Артур", "Кларк");
+        Book imRobot = new Book("Я Робот", 1950, azimov);
+        Book spaceOddity = new Book("Космическая одиссея", 1968, clarke);
+        System.out.printf("%s-%s %s%n%s", imRobot.getTitle(), imRobot.getAuthor().getFirstName(), imRobot.getAuthor().getSurname(), imRobot.getYear());
+        System.out.printf(   "" +"%n%s-%s %s%n%s", spaceOddity.getTitle(), spaceOddity.getAuthor().getFirstName(), spaceOddity.getAuthor().getSurname(), spaceOddity.getYear());
+        spaceOddity.setYear(2001);
+        System.out.println(" "+spaceOddity.getYear());
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
-}
+
+    }
